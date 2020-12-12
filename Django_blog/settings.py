966189e6d745a3e15b41d99dd190002ef5b3d237
jonathan-mwenze <jonathanmwenze@gmail.com>
2020-12-12 11:32:20 +0200

    users app

--- Django_blog/settings.py
+++ Django_blog/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
+    'users.apps.UsersConfig',
+    
 ]
 
 MIDDLEWARE = [
@@ -119,3 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+CRISPY_TEMPLATES_PACK = 'bootstrap4'
+
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 
 def index(request):
-	return render(request, 'blog/index.html')
+	context = {
+	   'posts': Post.objects.all()
+	}
+	return render(request, 'blog/index.html', context)
 
 def about(request):
 	return render(request, 'blog/about.html', {'title':'about'})
